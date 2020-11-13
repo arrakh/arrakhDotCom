f48b177af6762abe00242f0d9b53087cfaa61ecf
arrakh <arrakhpra@gmail.com>
2020-11-13 23:44:12 +0700

    Test template init

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://arrakh.github.io/",
+    "name": "Test Page",
+    "role": "Game Developer",
+    "linkedinId":"arrakh",
+    "skypeid": "aryarakha",
+    "roleDescription": "I'm just trying to test this template please don't sue me.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I'm just trying to test this template please don't sue me.",
+    "address":"Earth",
+    "website":"arrakh.com",
+    "education":[
+      {
+        "UniversityName":"The Institute of University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Definitely NOT a turd sniffer",
+        "name":"Not Kris"
+      },
+      {
+        "description":"what?",
+        "name":"Socrates"
+      }
+    ]
+  }
+  
+  export default resumeData
