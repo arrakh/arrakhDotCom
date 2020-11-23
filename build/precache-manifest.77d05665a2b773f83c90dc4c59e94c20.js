7ae80dac9a192e0409d52c1a2172da44a0302a98
Enjoy2Live <iMohammedMaqbol@gmail.com>
2020-11-23 14:38:04 +0300

    test

---
+++ build/precache-manifest.77d05665a2b773f83c90dc4c59e94c20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "73e6e0d5408ca98379cb",
+    "url": "/arrakhDotCom/static/css/main.6d64f976.chunk.css"
+  },
+  {
+    "revision": "73e6e0d5408ca98379cb",
+    "url": "/arrakhDotCom/static/js/main.73e6e0d5.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/arrakhDotCom/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "b29ea989e031e87d5e2c",
+    "url": "/arrakhDotCom/static/js/runtime~main.b29ea989.js"
+  },
+  {
+    "revision": "f2f4b5761e1d227e0c1c4c5d206b58d9",
+    "url": "/arrakhDotCom/index.html"
+  }
+];
